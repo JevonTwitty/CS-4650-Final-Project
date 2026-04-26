a8c83577c8a6d3f25d0f08a07394a9cc577271b1
Jevon Twitty <twittyjevon@gmail.com>
2026-04-26 18:24:19 -0400

    Small, temporary fix for dataloader, logistic regression implmementation

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -3,4 +3,6 @@ import pandas as pd
 def load_dataset(foldername='data', filename='AI_Detection'):
     file_path = f"{foldername}/{filename}.csv"
     data = pd.read_csv(file_path)
+    data.columns = 'text', 'label'
+
     return data
