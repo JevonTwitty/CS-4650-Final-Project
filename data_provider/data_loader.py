948841381980135010d9e0f13d7b1cbcab56e42d
Jevon Twitty <twittyjevon@gmail.com>
2026-04-27 21:00:42 -0400

    Implement LSTM, refactor code for easier configuration

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
 def load_dataset(foldername='data', filename='AI_Detection'):
+    """
+    This is a little scuffed.
+    If any dataset doesn't start with 2 columns in the order (text, label), this will break.
+    """
     file_path = f"{foldername}/{filename}.csv"
     data = pd.read_csv(file_path)
+
+    data = data.iloc[:, :2]  # Keep only the first two columns
     data.columns = 'text', 'label'
 
     return data
